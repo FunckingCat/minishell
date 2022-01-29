e1caeef61a1a4b42b537297279a2077760052ede
get-it-girl <nastia.gawrish@mail.ru>
2022-01-29 18:11:16 +0300

    correct error output

--- builtin/export.c
+++ builtin/export.c
@@ -13,6 +13,31 @@ void	clean_split(char **new_var)
 	free(new_var);
 }
 
+
+int check_name(char **extra)
+{
+	int i;
+
+	i = 0;
+	if (!extra || !extra[0])
+		return (1);
+	while (extra[0][i] != '\0')
+	{
+		if ((extra[0][0] > 47 && extra[0][0] < 58)
+			|| (!(extra[0][i] > 47 && extra[0][i] < 58)
+			&& !(extra[0][i] > 64 && extra[0][i] < 91)
+			&& !(extra[0][i] > 96 && extra[0][i] < 123)
+			&& extra[0][i] != '_'))
+		{
+			clean_split(extra);
+			put_error(EXP, EXP_NOT_VALID);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 int	setting_element(t_env *envi, char *str)
 {
 	char *new;
@@ -20,8 +45,8 @@ int	setting_element(t_env *envi, char *str)
 
 	new = ft_strchr(str, '=');
 	extra = ft_split(str, '=');
-	if (!extra || !extra[0])
-		return (1);
+	if (check_name(extra) == 1)
+		return (0);
 	if (new)
 		new = ft_strdup(&new[1]);
 	else
@@ -42,8 +67,8 @@ int	set_good_elem(t_env *envi, char **new, int i)
 {
 	if (new[i][0] == '=')
 	{
-		clean_split(new);
-		return (put_error(EXP, EXP_NOT_VALID));
+		put_error(EXP, EXP_NOT_VALID);
+		return (0);
 	}
 	else
 	{
@@ -53,39 +78,35 @@ int	set_good_elem(t_env *envi, char **new, int i)
 			return (1);
 		}
 		else
-		{
-			clean_split(new);
 			return (0);
-		}
 	}
 }
 
-
-int separating(t_env *envi, char *str)
+int	export(t_env *envi, char *str)
 {
 	char **new;
 	int i;
 
 	i = 0;
+	if (!str)
+		return (1);
 	if (ft_strchr(str, '=') == NULL)
+	{
+		check_args(str);
 		return (0);
+	}
 	new = ft_split(str, ' ');
 	while (new && new[i])
 	{
 		if (ft_strchr(new[i], '=') != NULL)
 		{
-			return (set_good_elem(envi, new, i));
+			if (set_good_elem(envi, new, i) == 1)
+				return (1);
 		}
 		else
-			i++;
+			check_args(new[i]);
+		i++;
 	}
 	clean_split(new);
 	return (0);
-}
-
-int	export(t_env *envi, char *str)
-{
-	if (!str)
-		return (1);
-	return (separating(envi, str));
 }
--- builtin/unset.c
+++ builtin/unset.c
@@ -1,6 +1,44 @@
 #include "builtin.h"
 
-int	unset(t_env *envi, char *name)
+void check_args(char *extra)
 {
-	return (env_delete(name, envi));
+	int i;
+
+	i = 0;
+	while (extra[i] != '\0')
+	{
+		if ((extra[0] > 47 && extra[0] < 58)
+			|| (!(extra[i] > 47 && extra[i] < 58)
+			&& !(extra[i] > 64 && extra[i] < 91)
+			&& !(extra[i] > 96 && extra[i] < 123)
+			&& extra[i] != '_'))
+		{
+			put_error(EXP, EXP_NOT_VALID);
+			return ;
+		}
+		i++;
+	}
+}
+
+int	unset(t_env *envi, char *args)
+{
+	char **names;
+	int i;
+
+	i = 0;
+	names = ft_split(args, ' ');
+	if (!names)
+		return (1);
+	while (names[i])
+	{
+		check_args(names[i]);
+		if (env_delete(names[i], envi) == 1)
+		{
+			clean_split(names);
+			return (1);
+		}
+		i++;
+	}
+	clean_split(names);
+	return (0);
 }
