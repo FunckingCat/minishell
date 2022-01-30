afbd6fbc8a39676b0f2c6a44d9a95585584eb980
David Zhuravlev <david99111@mial.ru>
2022-01-30 20:09:51 +0300

    redirect_error_to_stderr

--- error/error.c
+++ error/error.c
@@ -2,19 +2,31 @@
 
 int	put_error(char *module, char *text)
 {
-	printf(RED"%s: "NONE"%s\n", module, text);
+	ft_putstr_fd(RED, 2);
+	ft_putstr_fd(module, 2);
+	ft_putstr_fd(": " NONE, 2);
+	ft_putstr_fd(text, 2);
+	ft_putstr_fd("\n", 2);
 	return (1);
 }
 
 int	put_error_status(char *module, char *text, int st)
 {
-	printf(RED"%s: "NONE"%s\n", module, text);
+	ft_putstr_fd(RED, 2);
+	ft_putstr_fd(module, 2);
+	ft_putstr_fd(": " NONE, 2);
+	ft_putstr_fd(text, 2);
+	ft_putstr_fd("\n", 2);
 	return (st);
 }
 
 void	*put_error_null(char *module, char *text)
 {
-	printf(RED"%s: "NONE"%s\n", module, text);
+	ft_putstr_fd(RED, 2);
+	ft_putstr_fd(module, 2);
+	ft_putstr_fd(": " NONE, 2);
+	ft_putstr_fd(text, 2);
+	ft_putstr_fd("\n", 2);
 	return (NULL);
 }
 
