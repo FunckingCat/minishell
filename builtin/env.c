e019793423eb5e497b3d04b3630ca0205e093eee
David Zhuravlev <david99111@mial.ru>
2022-02-04 16:51:59 +0300

    env_rewrite

--- builtin/env.c
+++ builtin/env.c
@@ -1,15 +1,17 @@
 #include "builtin.h"
 
-int	env(t_env *envi)
+int		cmd_env(char **args, t_env *env);
 {
 	int	i;
 
 	i = 0;
-	if (!envi || !envi->vars)
-		return (1);
-	while (i < envi->length)
+	if (!env || !env->vars)
+		return (put_error(ENV, "env is corrupted"));
+	if (check_amount(args) > 0)
+		return (put_error(ENV, CD_MN_ARGS));
+	while (i < env->length)
 	{
-		ft_putstr_fd(envi->vars[i], 1);
+		ft_putstr_fd(env->vars[i], 1);
 		write(1, "\n", 1);
 		i++;
 	}
