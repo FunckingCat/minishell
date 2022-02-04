9913223d0f740d5a542d43ddade32dd3323b56e6
David Zhuravlev <david99111@mial.ru>
2022-02-04 17:16:43 +0300

    stage

--- builtin/export.c
+++ builtin/export.c
@@ -20,25 +20,22 @@ int check_name(char **extra)
 
 	i = 0;
 	if (!extra || !extra[0])
-		return (1);
+		return (put_error(EXP, EXP_NOT_VALID));
+	if (!ft_isalpha(extra[0][0]))
+		return (put_error(EXP, EXP_NOT_VALID));
 	while (extra[0][i] != '\0')
 	{
-		if ((extra[0][0] > 47 && extra[0][0] < 58)
-			|| (!(extra[0][i] > 47 && extra[0][i] < 58)
-			&& !(extra[0][i] > 64 && extra[0][i] < 91)
-			&& !(extra[0][i] > 96 && extra[0][i] < 123)
-			&& extra[0][i] != '_'))
+		if (!ft_isalnum(extra[0][i]) && extra[0][i] != '_')
 		{
 			clean_split(extra);
-			put_error(EXP, EXP_NOT_VALID);
-			return (1);
+			return (put_error(EXP, EXP_NOT_VALID));
 		}
 		i++;
 	}
 	return (0);
 }
 
-int	setting_element(t_env *envi, char *str)
+int	set_element(t_env *envi, char *str)
 {
 	char *new;
 	char **extra;
@@ -65,50 +62,31 @@ int	setting_element(t_env *envi, char *str)
 	return (0);
 }
 
-int	set_good_elem(t_env *envi, char **new, int i)
+int	set_good_elem(t_env *envi, char *arg)
 {
-	if (new[i][0] == '=')
-	{
-		put_error(EXP, EXP_NOT_VALID);
-		return (0);
-	}
+	if (arg[0] == '=')
+		return (put_error_status(EXP, EXP_NOT_VALID, 0));
+	else if (set_element(envi, arg))
+		return (1);
 	else
-	{
-		if (setting_element(envi, new[i]))
-		{
-			clean_split(new);
-			return (1);
-		}
-		else
-			return (0);
-	}
+		return (0);
 }
 
-int	export(t_env *envi, char *str)
+int	cmd_export(char **args, t_env *env)
 {
-	char **new;
 	int i;
 
 	i = 0;
-	if (!str)
-		return (1);
-	if (ft_strchr(str, '=') == NULL)
+	while (args && args[i])
 	{
-		check_args(str);
-		return (0);
-	}
-	new = ft_split(str, ' ');
-	while (new && new[i])
-	{
-		if (ft_strchr(new[i], '=') != NULL)
+		if (ft_strchr(args[i], '='))
 		{
-			if (set_good_elem(envi, new, i) == 1)
+			if (set_good_elem(env, args[i]) == 1)
 				return (1);
 		}
 		else
-			check_args(new[i]);
+			check_args(args[i]);
 		i++;
 	}
-	clean_split(new);
 	return (0);
 }
