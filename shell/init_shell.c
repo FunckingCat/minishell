4f8651d3bd39aaf03840761d4b0e64970128ae66
David Zhuravlev <david99111@mial.ru>
2022-02-05 13:57:25 +0300

    shelllevel_added

--- shell/init_shell.c
+++ shell/init_shell.c
@@ -2,12 +2,16 @@
 
 int	init_shell(t_shell *shell, char **envp)
 {
+	char	*buf;
+
 	shell->exit = 0;
 	shell->cmds = 0;
 	shell->cmds_arr = NULL;
 	shell->env = env_init(envp);
 	if (!shell->env)
 		return (1);
+	buf = ft_itoa(ft_atoi(env_get("SHLVL", shell->env)) + 1);
+	env_set("SHLVL", buf, shell->env);
 	return (0);
 }
 
